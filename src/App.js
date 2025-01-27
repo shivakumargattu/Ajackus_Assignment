a2eef6eea719b7c61769c2ba8cc626e82053e5cc
shiva kumar gattu <gshiva0018@gmail.com>
2025-01-27 20:23:54 +0530

    added Emplyee table

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
 
 import './App.css';
+import {BrowserRouter, Route, Routes}  from "react-router-dom"
+import User from './componets/User';
 
 function App() {
   return (
     <div className="App">
-      
+    
+    <h1>Empolye data crund</h1>
+    <BrowserRouter>
+      <Routes>
+
+         <Route path="/" element={<User/>}/>
+
+      </Routes>
+    </BrowserRouter>
+
     </div>
   );
 }
--- src/componets/User.js
+++ src/componets/User.js
@@ -3,7 +3,28 @@ import React from 'react'
 const User = () => {
   return (
     <div>
-     <h1>Hellow</h1>
+     <div className="container">
+        <div className="card">
+            <div className="card-title">
+                <h2>Employee Listing </h2>
+
+            </div>
+            <div className="card-body">
+
+                <table className="table table-boarder">
+                      <thead className="bg-dark text-white">
+                       <tr>
+                        <td>ID</td>
+                        <td>Name</td>
+                        <td>Email</td>
+                        <td>phone</td>
+                        <td>Role</td>
+                       </tr>
+                      </thead>
+                </table>
+            </div>
+        </div>
+     </div>
     </div>
   )
 }
