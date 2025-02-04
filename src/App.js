ee3ad6ada7e0b63db179e3c3f32b91635c143d44
shiva kumar gattu <gshiva0018@gmail.com>
2025-02-04 07:38:06 +0530

    updated code

--- src/App.js
+++ src/App.js
@@ -1,36 +1,15 @@
-<<<<<<< HEAD
 
-import './App.css';
-import {BrowserRouter, Route, Routes}  from "react-router-dom"
-import User from './componets/User';
-=======
-import logo from './logo.svg';
 import './App.css';
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import EmpListing from './EmpListing';
 import EmpCreate from './EmpCreate';
 import EmpDetail from './EmpDetail';
 import EmpEdit from './EmpEdit';
->>>>>>> 17b1234 (updetd code)
+
 
 function App() {
   return (
     <div className="App">
-<<<<<<< HEAD
-    
-    <h1>Empolye data crund</h1>
-    <BrowserRouter>
-      <Routes>
-
-         <Route path="/" element={<User/>}/>
-
-      </Routes>
-    </BrowserRouter>
-
-    </div>
-  );
-=======
-      
       <BrowserRouter>
         <Routes>
           <Route path='/' element={<EmpListing />}></Route>
@@ -43,7 +22,7 @@ function App() {
     </div>
   );
 
->>>>>>> 17b1234 (updetd code)
+
 }
 
 export default App;
