267f64c8d1633a6d6f2b277b76fc6d806114668e
shiva kumar gattu <gshiva0018@gmail.com>
2025-02-04 07:14:53 +0530

    updetd code

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
+<<<<<<< HEAD
 
 import './App.css';
 import {BrowserRouter, Route, Routes}  from "react-router-dom"
 import User from './componets/User';
+=======
+import logo from './logo.svg';
+import './App.css';
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import EmpListing from './EmpListing';
+import EmpCreate from './EmpCreate';
+import EmpDetail from './EmpDetail';
+import EmpEdit from './EmpEdit';
+>>>>>>> 17b1234 (updetd code)
 
 function App() {
   return (
     <div className="App">
+<<<<<<< HEAD
     
     <h1>Empolye data crund</h1>
     <BrowserRouter>
@@ -18,6 +29,21 @@ function App() {
 
     </div>
   );
+=======
+      
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element={<EmpListing />}></Route>
+          <Route path='/employee/create' element={<EmpCreate />}></Route>
+
+          <Route path='/employee/detail/:empid' element={<EmpDetail />}></Route>
+          <Route path='/employee/edit/:empid' element={<EmpEdit />}></Route>
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
+
+>>>>>>> 17b1234 (updetd code)
 }
 
 export default App;
